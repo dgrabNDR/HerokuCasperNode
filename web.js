6e624a34f90a9891fe8665009dea98a4be23feff
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-06-29 15:18:36 -0400

    Update web.js

--- web.js
+++ web.js
@@ -71,6 +71,11 @@ function startSpooky(){
 app.use(express.logger());
 app.get('/', function(request, response) {
     console.log('get');
+    startSpooky();
+    response.send(gGreeting);
+});
+app.post('/', function(request, response) {
+    console.log('post');
 	console.log(request);
     startSpooky();
     response.send(gGreeting);
