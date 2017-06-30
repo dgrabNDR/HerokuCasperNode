3e3d37bb39104e4f8eb29a08ca483e77272a6490
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-06-30 16:38:12 -0400

    Update web.js

--- web.js
+++ web.js
@@ -73,7 +73,7 @@ function startSpooky(searchTerm){
 				}
 			});
 			
-			spooky.waitForSelector(function(){'.aw-diagnostic-preview-iframe-v2'){
+			spooky.waitForSelector(function(){'.aw-diagnostic-preview-iframe-v2')
 				console.log('iframe found');
 			});
 			spooky.run();
