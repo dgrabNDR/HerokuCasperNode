8d23a94320db6b236dd052c01c8df4ee69198924
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-07-28 11:04:33 -0400

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,6 @@ var app = express();
 var Spooky = require('spooky');
 var bodyParser = require('body-parser');
 var jQuery = require('jquery');
-//var nforce = require('nforce');
 
 // adoped from Heroku's [Getting Started][] and [Spooky][]'s sample
 // [Getting Started]: https://devcenter.heroku.com/articles/getting-started-with-nodejs
